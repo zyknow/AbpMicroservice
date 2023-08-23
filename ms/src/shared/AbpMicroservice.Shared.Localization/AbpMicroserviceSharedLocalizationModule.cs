28122263abba8945f6a9e1db31dbafaa446b67eb
zyknow <1047606598@qq.com>
2023-08-23 18:45:18 +0800

    feat: add all language support

--- ms/src/shared/AbpMicroservice.Shared.Localization/AbpMicroserviceSharedLocalizationModule.cs
+++ ms/src/shared/AbpMicroservice.Shared.Localization/AbpMicroserviceSharedLocalizationModule.cs
@@ -22,10 +22,24 @@ public class AbpMicroserviceSharedLocalizationModule : AbpModule
 
         Configure<AbpLocalizationOptions>(options =>
         {
+            options.Languages.Add(new LanguageInfo("ar", "ar", "العربية", "ae"));
+            options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
+            options.Languages.Add(new LanguageInfo("en", "en", "English", "gb"));
+            options.Languages.Add(new LanguageInfo("en-GB", "en-GB", "English (UK)"));
+            options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
+            options.Languages.Add(new LanguageInfo("hr", "hr", "Croatian"));
+            options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish", "fi"));
+            options.Languages.Add(new LanguageInfo("fr", "fr", "Français", "fr"));
+            options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi", "in"));
+            options.Languages.Add(new LanguageInfo("it", "it", "Italiano", "it"));
+            options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+            options.Languages.Add(new LanguageInfo("ru", "ru", "Русский", "ru"));
+            options.Languages.Add(new LanguageInfo("sk", "sk", "Slovak", "sk"));
+            options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe", "tr"));
             options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-            options.Languages.Add(new LanguageInfo("en", "en", "English"));
-
-
+            options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
+            options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch", "de"));
+            options.Languages.Add(new LanguageInfo("es", "es", "Español"));
 
             options.Resources
                 .Add<AbpMicroserviceResource>("en")
